5b75189badef636b7bc2af329b330438cadb639c
Austin Tian <adt339@nyu.edu>
2024-05-02 13:04:54 -0400

    4.17: blogs now contain info on the user who adds the blog

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
     response.json(users)
 })
 
@@ -13,7 +13,7 @@ usersRouter.post('/', async (request, response) => {
     if (password === undefined || password.length < 3) {
         return response.status(400).json({ error: 'invalid password' })
     }
-    
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
